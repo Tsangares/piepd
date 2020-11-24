546d2e15e4e62ec32998ab013f795fb512ec7f2e
tsangares <tsangares@gmail.com>
2020-11-24 10:04:32 +0000

    init

---
+++ fonts.py
@@ -0,0 +1,6 @@
+import os
+fontPath = 'fonts/'
+castoro = os.path.join(fontPath,'castoro/Castoro-Regular.ttf')
+texturina = os.path.join(fontPath,'texturina/Texturina-Regular-opsz=24.ttf')
+inconsolata = os.path.join(fontPath,'inconsolata/static/Inconsolata-Black.ttf')
+nova = os.path.join(fontPath,'nova/NovaMono-Regular.ttf')
