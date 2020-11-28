b430f0fc1a03ad8115d1027213fa6b1cbd2a6224
William Wyatt <tsangares@gmail.com>
2020-11-28 00:38:14 -0800

    Fixed epd

--- piepd/fonts.py
+++ piepd/fonts.py
@@ -1,6 +1,8 @@
 import os,pathlib
-fontPath = os.path.join(pathlib.Path(__file__).parent.absolute(),'fonts/')
+nova = os.path.join(os.path.dirname(__file__),'fonts/nova/NovaMono-Regular.ttf')
+"""
 castoro = os.path.join(fontPath,'castoro/Castoro-Regular.ttf')
 texturina = os.path.join(fontPath,'texturina/Texturina-Regular-opsz=24.ttf')
 inconsolata = os.path.join(fontPath,'inconsolata/static/Inconsolata-Black.ttf')
 nova = os.path.join(fontPath,'nova/NovaMono-Regular.ttf')
+"""
